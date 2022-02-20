ecb1b93cb7ccf2fcdd03cfd90c0e7834e6a66cf4
Matheus Candido da Silva <matthewcsilva@gmail.com>
2022-02-20 17:53:27 -0300

    Creating new endpoints and storing/retrieving data from mongodb.

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,27 +1,70 @@
 import * as express from 'express';
+import Controller from '../interfaces/controller.interface';
 import Transaction from './transaction.interface';
+import transactionModel from './transactions.model';
  
-class PostsController {
-  public path = '/transactions';
-  public router = express.Router();
- 
-  constructor() {
-    this.intializeRoutes();
-  }
- 
-  public intializeRoutes() {
-    this.router.get(this.path, this.getAllTransactions);
-    this.router.post(this.path, this.createATransaction);
-  }
- 
-  getAllTransactions = (request: express.Request, response: express.Response) => {
-    response.send({});
-  }
- 
-  createATransaction = (request: express.Request, response: express.Response) => {
-    const post: Transaction = request.body;
-    response.send(post);
-  }
+class PostsController implements Controller {
+    public path = '/transactions';
+    public router = express.Router();
+    private transaction = transactionModel;
+
+    constructor() {
+        this.intializeRoutes();
+    }
+
+    public intializeRoutes() {
+        this.router.get(this.path, this.getAllTransactions);
+        this.router.get(`${this.path}/:id`, this.getTransactionById);
+        this.router.get(`${this.path}/category/:category`, this.getTransactionByCategory);
+        this.router.patch(`${this.path}/:id`, this.modifyTransaction);
+        this.router.delete(`${this.path}/:id`, this.deleteATransaction);
+        this.router.post(this.path, this.createATransaction);
+    }
+
+    private getAllTransactions = async (request: express.Request, response: express.Response) => {
+        const transactions = await this.transaction.find().exec();
+        response.send(transactions);
+    }
+
+    private getTransactionById = async (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        const transaction = await this.transaction.findById(id).exec();
+        response.send(transaction);
+    }
+
+    private getTransactionByCategory = async (request: express.Request, response: express.Response) => {
+        const category = request.params.category;
+        const transactions = await this.transaction.find({ category: `${category}`}).exec();
+        response.send(transactions);
+    }
+
+    private modifyTransaction = async (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        const data: Transaction = request.body;
+        const transaction = await this.transaction.findByIdAndUpdate(id, data, { new: true}).exec();
+        response.send(transaction);
+    }   
+
+    private createATransaction = async (request: express.Request, response: express.Response) => {
+        const transactionData: Transaction = request.body;
+        const newTransaction = new this.transaction(transactionData);
+        newTransaction.save()
+            .then( savedTransaction => {
+                response.send(savedTransaction);
+            })
+    }
+
+    private deleteATransaction = (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        this.transaction.findByIdAndDelete(id)
+            .then( successResponse => {
+                if (successResponse) {
+                    response.send(200);
+                } else {
+                    response.send(404);
+                }
+            })
+    }
 }
  
 export default PostsController;
