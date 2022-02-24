a17fe71368dceef49cdc3ab2b9cdb330b32d754a
Matheus Candido da Silva <matthewcsilva@gmail.com>
2022-02-23 22:29:24 -0300

    Creating actions for transaction view.

--- client/src/transactions/transaction.viewmodel.ts
+++ client/src/transactions/transaction.viewmodel.ts
@@ -1,25 +1,65 @@
-import { observable, action } from "mobx";
+import { observable, action, makeObservable, toJS } from "mobx";
+import React from 'react';
 import api from "../utils/api"
 import Transaction from "./transaction.model";
+import { SelectChangeEvent } from '@mui/material/Select';
 
 class TransactionViewModel {
-    @observable transactions: Array<Transaction> = [new Transaction("","",0)];
+    @observable transactions?: Array<Transaction>;
+    @observable searchField: string = "category";
+    @observable minValue: string = "0";
+    @observable maxValue: string = Number.MAX_SAFE_INTEGER.toString();
 
     constructor() {
+        makeObservable(this);
         this.load();
     }
 
     @action
-    public filter = () => {
-        
+    public search = async (e: Event) => {
+        const field:string = (<HTMLInputElement>e.target).id;
+        const v:string = (<HTMLInputElement>e.target).value;
+        if (!["min-value", "max-value"].includes(field)) {
+            if (!v) {
+                this.transactions = await (await api.get("/transactions")).data;
+            } else {
+                api.get(`/transactions/search/${field}/${v}`).then( (response) => {
+                    this.transactions = response.data;
+                }, error => {
+                    this.transactions = new Array<Transaction>();
+                })
+            }
+        } else {
+            if (field === "min-value") {
+                this.minValue = v;
+            } else {
+                this.maxValue = v;
+            }
+            api.get(`transactions/search/value/${this.minValue || 0}/${this.maxValue || Number.MAX_SAFE_INTEGER.toString()}`).then( (response) => {
+                this.transactions = response.data;
+            }, error => {
+                this.transactions = new Array<Transaction>();
+            })
+        }      
+    }
+
+    @action
+    public handleSelectChange = (event: SelectChangeEvent) => {
+        this.searchField = event.target.value;
+    };
+
+    @action
+    public save = async (transaction: {category: string, description: string, value: number}) => {
+        new Promise( async () => {
+            this.transactions = await (await api.post("/transactions", JSON.stringify(transaction), {headers: {"Content-Type": "application/json"}})).data;
+        })     
     }
     
     @action
     public load = async () => {
-        const transactions: Array<Transaction> = await (await api.get("/transactions")).data;
-        this.transactions = transactions;
-        console.log(transactions);
+        this.transactions = await (await api.get("/transactions")).data;
     }
+    
 }
 
 export default TransactionViewModel;
